aeb68b839ee9656ac3b0c57b288818c1891bee6c
Emanuel Jabon <138291892+vv4nity@users.noreply.github.com>
2025-08-17 19:58:50 +0800

    Add files via upload

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a>Facebook</a>
+        <a>Instagram</a>
+        <a>LinkedIn</a>
+    </div>
+  )
+}
