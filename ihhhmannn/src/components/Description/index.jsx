a4b371de462e4cdd2553f57d0f7c541e11b59b95
Emanuel Jabon <138291892+vv4nity@users.noreply.github.com>
2025-08-17 19:56:07 +0800

    Add files via upload

---
+++ ihhhmannn/src/components/Description/index.jsx
@@ -0,0 +1,62 @@
+import styles from './style.module.scss';
+import { useInView, motion } from 'framer-motion';
+import { useRef } from 'react';
+import { slideUp, opacity } from './animation';
+import Rounded from '../../common/RoundedButton';
+export default function index() {
+
+    const phrase = "Helping brands to stand out in the digital era. Together we will set the new status quo. No nonsense, always on the cutting edge.";
+    const description = useRef(null);
+    const isInView = useInView(description);
+
+    return (
+        <div ref={description} className={styles.description}>
+            <div className={styles.body}>
+                <p>
+                {
+                    phrase.split(" ").map((word, index) => {
+                        return (
+                            <span key={index} className={styles.mask}>
+                                <motion.span 
+                                    variants={slideUp} 
+                                    custom={index} 
+                                    animate={isInView ? "open" : "closed"} 
+                                >
+                                    {word}
+                                </motion.span>
+                            </span>
+                        );
+                    })
+                }
+                </p>
+                <motion.p 
+                    variants={opacity} 
+                    animate={isInView ? "open" : "closed"}
+                >
+                    The combination of my passion for design, code & interaction positions me in a unique place in the web design world.
+                </motion.p>
+                <div data-scroll data-scroll-speed={0.1}>
+                    <Rounded className={styles.button}>
+                        <p>About me</p>
+                    </Rounded>
+                </div>
+            </div>
+
+            {/* Services Section */}
+            <div className={styles.services}>
+                <div>
+                    <h3>Web Design</h3>
+                    <p>User-friendly, tailored designs built with quality and intention.</p>
+                </div>
+                <div>
+                    <h3>Development</h3>
+                    <p>Robust websites built from scratch—focused on interactions and performance.</p>
+                </div>
+                <div>
+                    <h3>End-to-end Solution</h3>
+                    <p>From concept to launch—I craft complete digital experiences.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
